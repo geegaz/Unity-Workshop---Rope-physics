5bf3f02f0cec6637e4b6a9aeb9874efba57a6b86
Geegaz <geegazrd@gmail.com>
2022-11-23 14:07:13 +0100

    Working VerletRope force response

--- Assets/Scripts/VerletRope/VerletRope.cs
+++ Assets/Scripts/VerletRope/VerletRope.cs
@@ -6,15 +6,16 @@ public class VerletRope : MonoBehaviour
 {
     [Header("Rope")]
     [SerializeField]
-    private Transform attachPoint;
+    private Rigidbody attachedBody;
     [SerializeField]
-    private Rigidbody attachBody;
+    private float attachedBodiesDamping = 0.8f;
 
-    public Dictionary<int, Transform> attachPoints = new Dictionary<int, Transform>();
+    private SortedDictionary<int, Transform> pointsAttach = new SortedDictionary<int, Transform>();
+    private Dictionary<int, Vector3> pointsForce = new Dictionary<int, Vector3>();
     
-    Vector3[] pos;
-    Vector3[] prevPos;
-    float[] mass;
+    private Vector3[] pos;
+    private Vector3[] prevPos;
+    private float[] mass;
     
     public int pointsNb = 20;
 
@@ -30,24 +31,19 @@ public class VerletRope : MonoBehaviour
 
     private void Start() {
         CreatePoints();
-
-        attachPoints.Add(0, transform);
-        if (attachPoint) attachPoints.Add(pointsNb - 1, attachPoint);
     }
 
     private void FixedUpdate() {
         if (pointsNb > 1) {
             ApplyVerlet();
             ApplyConstraints();
+            
             ApplyAttach();
+            ApplyForces();
         }
 
         if (line)
             line.SetPositions(pos);
-        
-        if (attachBody)
-            mass[pointsNb - 1] = attachBody.mass;
-            attachBody.MovePosition(pos[pointsNb - 1]);
     }
 
     private void CreatePoints() {
@@ -55,21 +51,85 @@ public class VerletRope : MonoBehaviour
         prevPos = new Vector3[pointsNb];
         mass = new float[pointsNb];
 
-        Vector3 targetPos = attachPoint ? attachPoint.position : transform.position + Physics.gravity.normalized * (constraintDistance * pointsNb);
+        Vector3 targetPos = attachedBody ? attachedBody.position : transform.position + Physics.gravity.normalized * (constraintDistance * pointsNb);
         for (int i = 0; i < pointsNb; i ++) {
             pos[i] = Vector3.Lerp(transform.position, targetPos, (float)i / (pointsNb - 1));
             prevPos[i] = pos[i];
             mass[i] = 1.0f;
         }
 
+        CreateAttach(0, transform);
+        if (attachedBody) CreateAttach(pointsNb - 1, attachedBody.transform);
+
         if (line) line.positionCount = pointsNb;
     }
 
+    public void CreateAttach(int point, Transform attach) {
+        if (point < 0 || point >= pointsNb){
+            Debug.LogError(string.Format("Point {0} is outside of points amount {1}",point,pointsNb));
+            return;
+        }
+        if (pointsAttach.ContainsKey(point)) {
+            pointsAttach[point] = attach;
+            pointsForce[point] = Vector3.zero;
+            return;
+        }
+
+        pointsAttach.Add(point, attach);
+        pointsForce.Add(point, Vector3.zero);
+    }
+
+    public void RemoveAttach(int point) {
+        if (pointsAttach.ContainsKey(point)) {
+            pointsAttach.Remove(point);
+            pointsForce.Remove(point);
+        } else {
+            Debug.LogError(string.Format("No point {0} in attached points",point));
+        }
+    }
+
     private void ApplyAttach() {
-        foreach (int point in attachPoints.Keys)
+        Vector3 delta;
+        float distance;
+        float length;
+        float diff;
+        float invmass1;
+        float invmass2;
+        int previousPoint = -1;
+        foreach (int point in pointsAttach.Keys)
         {
-            if (point >= 0 && point < pointsNb) {
-                pos[point] = attachPoints[point].position;
+            pos[point] = pointsAttach[point].position;
+
+            if (previousPoint >= 0) {
+                // pointsAttach should be ordered by id
+                delta = pos[point] - pos[previousPoint];
+                distance = constraintDistance * (point - previousPoint);
+                length = delta.magnitude;
+                invmass1 = InverseMass(mass[previousPoint]);
+                invmass2 = InverseMass(mass[point]);
+                diff = (length - distance) / (length * (invmass1 + invmass2));
+                
+                if (diff > 0.0f) {
+                    pointsForce[previousPoint] += delta * diff * invmass1;
+                    pointsForce[point] = -delta * diff * invmass2;
+                } else {
+                    pointsForce[point] = Vector3.zero;
+                }
+            } else {
+                pointsForce[point] = Vector3.zero;
+            }
+            previousPoint = point;
+        }
+    }
+
+    private void ApplyForces() {
+        Rigidbody body;
+        foreach (int point in pointsAttach.Keys) {
+            body = pointsAttach[point].GetComponent<Rigidbody>();
+            if (body) {
+                mass[point] = body.mass;
+                body.velocity += pointsForce[point];
+                body.position += (body.velocity) * Time.deltaTime;
             }
         }
     }
@@ -88,27 +148,23 @@ public class VerletRope : MonoBehaviour
         Vector3 delta;
         float length;
         float diff;
-        // float invmass1;
-        // float invmass2;
+        float invmass1;
+        float invmass2;
         for (int iteration = 0; iteration < constraintIterations; iteration++) {
             for (int i = 1; i < pointsNb; i++) {
                 delta = pos[i] - pos[i - 1];
                 length = delta.magnitude;
-
-                diff = (length - constraintDistance) / length;
-                pos[i] -= delta * diff * 0.5f;
-                pos[i - 1] += delta * diff * 0.5f;
+                invmass1 = InverseMass(mass[i - 1]);
+                invmass2 = InverseMass(mass[i]);
+                diff = (length - constraintDistance) / (length * (invmass1 + invmass2));
                 
-                // invmass1 = InverseMass(mass[i - 1]);
-                // invmass2 = InverseMass(mass[i]);
-                // diff = (length - constraintDistance) / (length * (invmass1 + invmass2));
-                // pos[i] -= delta * diff * invmass2;
-                // pos[i - 1] += delta * diff * invmass1;
+                pos[i - 1] += delta * diff * invmass1;
+                pos[i] -= delta * diff * invmass2;
             }
         }
     }
 
     private static float InverseMass(float mass) {
-        return mass == 0.0f ? 1000000000.0f : 1.0f / mass;
+        return mass == 0.0f ? 0.00000001f : 1.0f / mass;
     }
 }
